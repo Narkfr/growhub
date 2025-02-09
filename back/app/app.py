7c5bdd6d4df9355944b779a614dea775c1583663
marius <marius@nark.fr>
2025-02-09 20:04:05 +0100

    trucs

--- back/app/app.py
+++ back/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from config import routes, env
 import paho.mqtt.client as mqtt
+import redis
 
 app = Flask(__name__)
 
@@ -11,14 +12,28 @@ MQTT_TOPIC = env.MQTT_TOPIC
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 
+# Redis Configuration
+REDIS_HOST = env.REDIS_HOST
+REDIS_PORT = env.REDIS_PORT
+redisServer = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
 # Connect to MQTT Broker
 def connect_mqtt():
     try:
         client.connect(MQTT_BROKER, MQTT_PORT)
         client.loop_start()
-        print("MQTT client connected")
-    except Exception as e:
-        print(f"Error connecting to MQTT broker: {e}")
+        print("✅ MQTT client connected")
+    except (mqtt.MQTTException, ConnectionRefusedError) as e:
+        print(f"❌ Error connecting to MQTT broker: {e}")
+
+# Connect to Redis
+def connect_redis():
+    try:
+        redisServer.ping()
+        print("✅ Connected to Redis successfully!")
+    except redis.ConnectionError:
+        print("❌ Failed to connect to Redis.")
+        exit(1)
 
 @app.route(routes.INDEX)
 def index():
@@ -44,6 +59,22 @@ def mqtt_post():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route(routes.REDIS, methods=['POST'])
+def redis_post():
+    try:
+        data = request.json
+        print(data)
+        key = data.get("key")
+        value = data.get("value")
+        print(f"Setting key: {key}, value: {value}")
+
+        # Set key-value pair in Redis
+        redisServer.set(key, value)
+        return jsonify({"key": key, "value": value})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     connect_mqtt()
+    connect_redis()
     app.run(debug=True, host='0.0.0.0')
