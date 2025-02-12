88f144dbfdb20cc4b2bc1b27a36b9e9ac51b90f2
marius <marius@nark.fr>
2025-02-12 17:53:01 +0100

    improve test form

--- back/app/static/js/scripts.js
+++ back/app/static/js/scripts.js
@@ -1,26 +1,39 @@
-function scheduleMqttTask(topic, message) {
+function scheduleMqttTaskAtDelay(event) {
+    event.preventDefault(); // Prevent page reload
+
+    // Get form data
+    const formData = new FormData(event.target);
+    const topic = formData.get("topic");
+    const message = formData.get("message");
+    const delay = parseInt(formData.get("delay"), 10); // Convert to integer
     fetch('/schedule/mqtt', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ topic: topic, message: message, delay: 20 })
+        body: JSON.stringify({ topic: topic, message: message, delay: delay })
     }).then(response => response.json()).then(data => {
         console.log(data);
-        alert("MQTT message scheduled in 2 minutes!");
+        alert(`MQTT message scheduled in ${delay} seconds!`);
     });
 }
 
-function scheduleMqttTaskAtTime(topic, message) {
-    const datetime = new Date(document.getElementById('mqttScheduleTime').value)
+function scheduleMqttTaskAtTime(event) {
+    event.preventDefault();
+    console.log(event);
+    // Get form data
+    const formData = new FormData(event.target);
+    const topic = formData.get("topic");
+    const message = formData.get("message");
+    const datetime = new Date(formData.get("datetime"))
         .toISOString()
         .slice(0, 19)
-        .replace("T", " "); // "2025-02-09 21:30:00"
+        .replace("T", " "); // Format: "YYYY-MM-DD HH:MM:SS"
     fetch('/schedule/mqtt/datetime', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ topic: topic, message: message + datetime, datetime: datetime })
     }).then(response => response.json()).then(data => {
         console.log(data);
-        alert("MQTT message scheduled at " + datetime);
+        alert(`MQTT message scheduled at ${datetime}`);
     });
 }
 
